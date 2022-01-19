37cf9133524b296bdd9a79f62833173bb989cbd1
Erik Howley <howleye@ferris.edu>
2022-01-19 10:44:20 -0500

    3D NoiseField in Unity
    + SketchNoise3D scene
    + Chunk3DController script
    + Noise script

---
+++ ProceduralProject/Assets/Scripts/chunk3DController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chunk3DController : MonoBehaviour
+{
+    public GameObject voxelPrefab;
+
+    [Tooltip("How many voxels per dimension")]
+    public int dimensionSize = 5;
+
+    [Tooltip("Size of a voxel, in meters.")]
+    public float voxelSize = 1;
+
+    [Tooltip("Density threshold from 0 to 1")]
+    public float threshold = 0.5f;
+
+    [Tooltip("How much to scale the noisefield")]
+    public float zoom = 10;
+
+    private void Start()
+    {
+        GenerateVoxels();
+    }
+
+    private void OnValidate()
+    {
+        if (!Application.isPlaying) return;
+        GenerateVoxels();
+    }
+
+    private void GenerateVoxels()
+    {
+        if (!voxelPrefab) return;
+
+        // Delete existing voxels in the chunk
+        foreach(Transform child in transform){
+            Destroy(child.gameObject);
+        }
+
+        for (int x = 0; x < dimensionSize; x++)
+        {
+            for (int y = 0; y < dimensionSize; y++)
+            {
+                for (int z = 0; z < dimensionSize; z++)
+                {
+
+                    Vector3 pos = new Vector3(x, y, z) * voxelSize;
+
+                    float val = Noise.Perlin(pos / zoom);
+
+                    if (val > threshold)
+                    {
+                        GameObject obj = Instantiate(voxelPrefab, pos, Quaternion.identity, transform);
+                        obj.transform.localScale = Vector3.one * voxelSize;
+                    }
+                }
+            }
+        }
+    }
+}
